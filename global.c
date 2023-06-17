5a27cb8cf5661397832d3cd8eddaa8520f0e10eb
Jakub <jakub.witkowski@gmail.com>
2023-06-17 18:42:54 +0200

    Add global variables, structs, etc.

---
+++ global.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include "global.h"
+
+int coreNum = 0;
+
+double cpuUsage[];
---
+++ global.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+extern int coreNum;
+
+extern double cpuUsage[];
+
+struct cpuStats
+{
+    char name[7];
+    uint32_t user;
+    uint32_t nice;
+    uint32_t system;
+    uint32_t idle;
+    uint32_t iowait;
+    uint32_t irq;
+    uint32_t softirq;
+    uint32_t steal;
+    uint32_t guest;
+    uint32_t guest_nice;    
+};
